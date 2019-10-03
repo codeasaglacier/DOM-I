f9b1a3fdb03e2dee6a49ef9a36dac6ea20dec3d1
codeasaglacier <remingtonshock@gmail.com>
2019-10-02 16:05:26 -0800

    started digital timer

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,5 @@
+const msT = document.getElementById('msTens');
+const msH = document.getElementById('msHundreds')
+const sO = document.getElementById('secondOnes')
+const sT = document.getElementById('secondTens')
+
